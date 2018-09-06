f2972d691b1b380e73fb36dde55a375b8c6090f4
Kyle <johnsenkyle13@gmail.com>
2018-09-05 20:09:59 -0600

    Finish make-data scripts

---
+++ inst/scripts/make-data.R
@@ -0,0 +1,22 @@
+# SEE BASH SCRIPT FOR ALIGNMENT
+system('bash make-data.sh')
+
+library(Rsamtools)
+
+wtBam <- BamFile('zy13wt.bam')
+mutBam <- BamFile('zy13mut.bam')
+
+# SORT AND INDEX
+sortBam(wtBam)
+indexBam(wtBam)
+sortBam(mutBam)
+indexBam(mutBam)
+
+# FILTER AND CUT TO CHROMOSOME 7
+# TO REDUCE TO MINIMUM WORKING SIZE FOR EXAMPLES
+wtBam <- filterBam(wtBam, 'zy13wt.cut.bam',
+                   param = ScanBamParam(mapqFilter = 30,
+                                        which = GRanges('7')))
+mutBam <- filterBam(mutBam, 'zy13mut.cut.bam',
+                   param = ScanBamParam(mapqFilter = 30,
+                                        which = GRanges('7')))
--- inst/scripts/make-data.sh
+++ inst/scripts/make-data.sh
@@ -5,6 +5,11 @@
 # Click on `Download Files` and download the two .fastq files we need:
 # Zy13 WT (8187X1_110524_SN141_0355_AD0D99ABXX_1.txt.gz)
 # Zy13 MUT (8187X1_110524_SN141_0355_AD0D99ABXX_1.txt.gz)
+gunzip 8187X1_110524_SN141_0355_AD0D99ABXX_1.txt.gz
+mv 8187X1_110524_SN141_0355_AD0D99ABXX_1.txt zy13wt.fastq
+gunzip 8187X2_110524_SN141_0355_AD0D99ABXX_1.txt.gz
+mv 8187X2_110524_SN141_0355_AD0D99ABXX_1.txt zy13mut.fastq
+
 
 ### HISAT2 PREP
 # Get GRCz11 reference genome from Ensembl:
@@ -13,27 +18,34 @@ gunzip Danio_rerio.GRCz11.dna.primary_assembly.fa.gz
 mv Danio_rerio.GRCz11.dna.primary_assembly.fa GRCz11.fa
 
 # Construct HISAT2 index
-hisat2-build GRCz11.fa GRCz11
+hisat2-build -f GRCz11.fa GRCz11
+## NOTE: IT WOULD HAVE BEEN PREFERABLE TO INCLUDE SPLICE
+## SITES IN THE INDEX
+
+# Get splice sites
+wget ftp://ftp.ensembl.org/pub/release-93/gtf/danio_rerio/Danio_rerio.GRCz11.93.gtf.gz
+gunzip Danio_rerio.GRCz11.93.gtf.gz
+mv Danio_rerio.GRCz11.93.gtf.gz GRCz11.gtf
+python hisat2_extract_splice_sites.py GRCz11.gtf > splice_sites_GRCz11.txt
 
 
 ### HISAT2 ALIGNMENT
 align () {
         hisat2 --no-unal \
-        --known-splicesite-infile splicesites_GRCz11.txt \
-        -x ../../hisat2_reference/GRCz11/GRCz11 \
-        -U ../"${1}.txt" \
+        --known-splicesite-infile splice_sites_GRCz11.txt \
+        -x GRCz11 \
+        -U "${1}.fastq" \
         -S "${1}.sam"
 }
 
 convert_to_bam () {
         samtools view -b -1 -o "${1}.bam" "${1}.sam"
-        #rm "${1}.hisat2-10.sam"
 }
 
-sort_bam () {
-        samtools sort -o "${1}.sorted.bam" "${1}.bam"
-        #rm "${1}.hisat2-10.bam"
-}
+### ALIGN FILES AND CONVERT TO BAM
+align zy13wt &&\
+convert_to_bam zy13wt
 
+align zy13mut &&\
+convert_to_bam zy13mut
 
-### PREPARE FILE
