fdaefc12fef2db1fcd49d8abf9c9c42442ec7068
Kyle <johnsenkyle13@gmail.com>
2018-10-17 08:40:11 -0600

    Move doc to data.R

--- R/MMAPPR2data.R
+++ R/MMAPPR2data.R
@@ -74,19 +74,3 @@ zy13wtBam <- function() {
     return(Rsamtools::BamFile(file.path(hubLocation, 'zy13wt.bam'),
                               file.path(hubLocation, 'zy13wt.bai')))
 }
-
-#' Candidates list for zy13 example
-#'
-#' A list representing the candidates info resulting from running
-#' the MMAPPR2 pipeline on the sample data contained in this
-#' package.
-#'
-#' The typical user should not have to concern themselves with this
-#' dataset; it is used to enable dynamic examples in the development of
-#' the MMAPPR2 package.
-#'
-#' @format A list containing only one element: a
-#' \code{\link[GenomicRanges]{GRanges}} object containing
-#' ranked candidate mutations resulting from the \code{generateCandidates}
-#' step of the MMAPPR2 pipeline.
-"zy13candidates"
