13e0fb7db2ceec49de612c39d35b7efa20060ca9
Kyle <johnsenkyle13@gmail.com>
2018-10-22 17:45:29 -0600

    Another docs link fix; bump to v0.99.8

--- R/MMAPPR2data.R
+++ R/MMAPPR2data.R
@@ -56,7 +56,7 @@ zy13mutBam <- function() {
 #'
 #' @describeIn MMAPPR2data Download wild-type BAM and index files
 #'   simultaneously. This is the
-#'   easiest way to use the data, especially in \link[MMAPPR2]{MMAPPR2}
+#'   easiest way to use the data, especially in MMAPPR2
 #'   examples.
 #' @return A \code{\link[Rsamtools:BamFile-class]{BamFile}} object referencing downloaded
 #'   BAM file and its index.
